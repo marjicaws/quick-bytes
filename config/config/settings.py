3ba6fdf3a452a04443123ca79427a0147524884c
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2022-03-09 16:23:18 -0500

    Starting to build models

--- config/config/settings.py
+++ config/config/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quick',
+        'USER': 'quick_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
 
