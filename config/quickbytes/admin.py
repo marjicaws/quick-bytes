32b9746ab345eaf93ee072ae625f9581c05e52df
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2022-03-14 16:32:13 -0400

    Model created

--- config/quickbytes/admin.py
+++ config/quickbytes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Byte
+
+admin.site.register(Byte)
 
 # Register your models here.
--- config/quickbytes/models.py
+++ config/quickbytes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+class Byte(models.Model):
+    meal_choices=(
+        ('Breakfast', 'Breakfast'),
+        ('Dinner', 'Dinner'),
+        ('Dessert', 'Dessert'),
+        ('Drinks','Drinks'),
+        ('Snacks', 'Snacks')
+
+
+    )
+    title = models.CharField(max_length=128)
+    recipe = models.CharField(max_length=500)
+    image = models.CharField(max_length=200)
+    ingredients = models.CharField(max_length=300)
+    category = models.CharField(max_length=50, choices=meal_choices)
+    author = models.CharField(max_length=128 , blank=False, default='quick byte user')
+
+    def __str__(self):
+        return self.title
+    
+    def __str__(self):
+        return self.author
+    
+
+
 # Create your models here.
